5509a5f1b1b80921e4ddeafe1d5d17e4735388eb
Kerttu Raudleht <kerttu.raudleht@tktk.ee>
2026-01-12 01:22:16 +0200

    “Week2”

---
+++ greeting.py
@@ -0,0 +1,11 @@
+perekonnanimi = input("Sisesta oma perekonnanimi:")
+
+sugu = input("Sisesta oma sugu (M/N):")
+
+if sugu == "M":
+ print(f"Tere, härra {perekonnanimi}!")
+elif sugu == "N":
+ print(f"Tere, proua {perekonnanimi}!")
+else:
+ print(f"Tere tulemast, {perekonnanimi}!")
+
